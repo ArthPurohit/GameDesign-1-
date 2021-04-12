f6e6b5f65d3466fe955a9a3bf56818b9db129574
ArthPurohit <77054530+ArthPurohit@users.noreply.github.com>
2021-04-12 17:55:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var Car , CarImg 
+var GasCan , GasCanImg
+var track , trackImg
+var rock , rockImg
+
+function preload(){
+
+ CarImg=loadImage
+
+
+
+}
+
+function setup(){
+
+
+}
+
+function draw(){
+
+
+}
